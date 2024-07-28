b6c457687eeec16f2ba32815cf088f83dda0244b
chr <chen_haoran2003@163.com>
2024-07-28 11:11:39 +0800

    Updata: defines.v file

--- defines.v
+++ defines.v
@@ -19,7 +19,25 @@
 //**********    macro define relate to Instructions
 `define EXE_ORI             6'b001101           // ori InstCode
 `define EXE_NOP             6'b000000
+`define EXE_AND             6'b100100
+`define EXE_OR              6'b100101
+`define EXE_XOR             6'b100110
+`define EXE_NOR             6'b100111
+`define EXE_ANDI            6'b001100
+`define EXE_ORI             6'b001101
+`define EXE_XORI            6'b001110
+`define EXE_LUI             6'b001111
 
+`define EXE_SLL             6'b000000
+`define EXE_SLLV            6'b000100
+`define EXE_SRL             6'b000010
+`define EXE_SRLV            6'b000110
+`define EXE_SRA             6'b000011
+`define EXE_SRAV            6'b000111
+
+`define EXE_SYNC            6'b001111
+`define EXE_PREF            6'b110011
+`define EXE_SPECIAL_INST    6'b000000
 
 //AluOp
 `define EXE_OR_OP           8'b00100101
